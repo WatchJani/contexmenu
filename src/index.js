4986927202233eaedac9ec58d003cd2d51748f38
Savo Kos <savo.kos.sk@gmail.com>
2022-03-05 14:33:21 +0100

    Formatted

--- src/index.js
+++ src/index.js
@@ -1,52 +1,47 @@
-import ReactDOM from "react-dom"
-import { ContextMenuView } from "./ContexMenuView/contextMenuView"
-import { ContextMenuButton } from "./ContextMenuControl/ContextMenuButton/contextMenuButton"
-import { ContextMenuCheckBox } from "./ContextMenuControl/ContextMenuCheckBox/contexMenuCheckBox"
-import { ContextMenuGroup } from "./ContextMenuControl/ContexMenuGroup/contexMenuGroup"
+import ReactDOM from 'react-dom';
+import { ContextMenuView } from './ContexMenuView/contextMenuView';
+import { ContextMenuButton } from './ContextMenuControl/ContextMenuButton/contextMenuButton';
+import { ContextMenuCheckBox } from './ContextMenuControl/ContextMenuCheckBox/contexMenuCheckBox';
+import { ContextMenuGroup } from './ContextMenuControl/ContexMenuGroup/contexMenuGroup';
 const App = () => {
+  const fja = () => {
+    alert('Asd');
+  };
 
-    const fja = () => {
-        alert("Asd")
-    }
+  const data = [
+    {
+      name: ContextMenuButton,
+      text: 'Skupi sve kategorije',
+      action: fja,
+    },
+    {
+      name: ContextMenuButton,
+      text: 'Uredi kategorije',
+      action: null,
+    },
+    {
+      name: ContextMenuButton,
+      text: 'Skupi sve kategorije',
+      action: fja,
+    },
+    {
+      name: ContextMenuButton,
+      text: 'Uredi kategorije',
+      action: null,
+    },
+    {
+      name: ContextMenuGroup,
+      text: null,
+      action: null,
+    },
+    {
+      name: ContextMenuCheckBox,
+      text: 'Skupi kategoriju',
+      action: null,
+    },
+  ];
 
+  return <ContextMenuView data={data} />;
+};
 
-    const data = [
-        {
-            name: ContextMenuButton,
-            text: "Skupi sve kategorije",
-            action: fja
-        },
-        {
-            name: ContextMenuButton,
-            text: "Uredi kategorije",
-            action: null
-        },
-        {
-            name: ContextMenuButton,
-            text: "Skupi sve kategorije",
-            action: fja
-        },
-        {
-            name: ContextMenuButton,
-            text: "Uredi kategorije",
-            action: null
-        },
-        {
-            name: ContextMenuGroup,
-            text: null,
-            action: null
-        },
-        {
-            name: ContextMenuCheckBox,
-            text: "Skupi kategoriju",
-            action: null
-        }
-    ]
-
-
-    return (
-        <ContextMenuView data={data} />
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById('app'));
